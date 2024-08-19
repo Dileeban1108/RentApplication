34a4e6ba5e792124ea734d56fb80846bd8970ebc
dileeban <dileebandileeban1108@gmail.com>
2024-08-19 14:44:17 +0530

    Add project files.

---
+++ RentApplication/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentApplication.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        [Column(TypeName = "nvarchar(10000)")]
+        public string Name { get; set; } = "";
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; } = "";
+        [Column(TypeName = "nvarchar(20)")]
+        public string Password { get; set; } = "";
+        [Column(TypeName = "nvarchar(10)")]
+        public int PhoneNumber { get; set; }
+        
+           
+    }
+}
